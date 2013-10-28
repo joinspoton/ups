5a9b1d6e63350d33265681eab6f3109c7b643c31
Teddy Cross <tkazec@gmail.com>
2013-10-28 15:02:45 -0700

    implement 2/3 of the tests!

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,22 @@
 var assert = require('assert');
+var crypto = require('crypto');
 var ff = require('ff');
-var fs = require('fs');
-var ups = require('ups');
+var shell = require('shelljs');
+var ups = require('..');
+
+var hash = function (data) {
+  return crypto.createHash('md5').update(data).digest('hex');
+};
+
+before(function () {
+  shell.cd(__dirname);
+  shell.rm('-r', 'publics');
+  shell.mkdir('publics');
+});
+
+after(function () {
+  shell.rm('-r', 'publics');
+});
 
 describe('ups.build()', function () {
   it('should create assets');
@@ -10,61 +25,108 @@ describe('ups.build()', function () {
   
   it('should customize group strings');
   
+  it('should compress assets');
+  
   it('should be deterministic');
 });
 
 describe('ups.clean()', function () {
+  beforeEach(function (next) {
+    shell.rm('-r', 'publics');
+    shell.mkdir('publics');
+    
+    ups.build(__dirname + '/configs/basic.json', false, next);
+  });
+  
   it('should delete extraneous files', function (next) {
     var f = ff(function () {
-      ups.clean('', f.wait());
+      ''.to('publics/foofile');
+      'a+b=c'.to('publics/xxx-old.asset');
+      
+      ups.clean(__dirname + '/configs/basic.json', f.wait());
     }, function () {
-      fs.readdir('', f.slot());
-    }, function (list) {
-      assert.deepEqual(list, []);
+      assert.deepEqual(shell.ls('-A', 'publics'), ['3a32d3bc8e-global.js', 'manifest.json']);
     }).onComplete(next);
   });
   
-  it('should ignore valid assets');
-  
-  it('should ignore the manifest');
-  
-  it('should ignore dotfiles');
-});
-
-describe('ups.types', function () {
-  it('should handle custom types');
+  it('should ignore valid assets, the manifest, and dotfiles', function (next) {
+    var f = ff(function () {
+      'dotdotdot'.to('publics/.dotfile');
+      ''.to('publics/.placeholder');
+      
+      ups.clean(__dirname + '/configs/basic.json', f.wait());
+    }, function () {
+      assert.deepEqual(shell.ls('-A', 'publics'), ['.dotfile', '.placeholder', '3a32d3bc8e-global.js', 'manifest.json']);
+    }).onComplete(next);
+  });
 });
 
 describe('ups/types', function () {
+  var manifest;
+  
+  before(function (next) {
+    var f = ff(function () {
+      ups.types.coffee = require('../types/coffeescript');
+      ups.types.handlebars = require('../types/handlebars');
+      ups.types.hbs = require('../types/handlebars-ember');
+      ups.types.less = require('../types/less');
+      ups.types.stylus = require('../types/stylus');
+      ups.types.styl = require('../types/stylus-nib');
+      
+      shell.rm('-r', 'publics');
+      shell.mkdir('publics');
+      
+      ups.build(__dirname + '/configs/types.json', false, f.wait());
+    }, function () {
+      manifest = JSON.parse(shell.cat('publics/manifest.json'));
+    }).onComplete(next);
+  });
+  
   describe('coffeescript', function () {
-    it('should render CoffeeScript');
+    it('should render CoffeeScript', function () {
+      assert.equal(manifest.all['coffeescript.js'], 'a638d3300f');
+    });
   });
   
   describe('css', function () {
-    it('should render CSS');
+    it('should render CSS', function () {
+      assert.equal(manifest.all['css.css'], '3672c5f0b6');
+    });
   });
   
   describe('handlebars', function () {
-    it('should render Handlebars');
+    it('should render Handlebars', function () {
+      assert.equal(manifest.all['handlebars.js'], 'd656caf90f');
+    });
   });
   
   describe('handlebars-ember', function () {
-    it('should render Emberized Handlebars');
+    it('should render Emberized Handlebars', function () {
+      assert.equal(manifest.all['ember.js'], '45b4077ffc');
+    });
   });
   
   describe('js', function () {
-    it('should render JS');
+    it('should render JS', function () {
+      assert.equal(manifest.all['js.js'], 'e3265f70cc');
+    });
   });
   
   describe('less', function () {
-    it('should render LESS');
+    it('should render LESS', function () {
+      assert.equal(manifest.all['less.css'], '20016d933a');
+    });
   });
   
   describe('stylus', function () {
-    it('should render Stylus');
+    it('should render Stylus', function () {
+      assert.equal(manifest.all['stylus.css'], '4bf19df04b');
+    });
   });
   
   describe('stylus-nib', function () {
-    it('should render Stylus with Nib');
+    it('should render Stylus with Nib', function () {
+      assert.equal(manifest.all['nib.css'], '7589b54712');
+    });
   });
 });
---
+++ tests/sources/js.js
@@ -0,0 +1,2 @@
+// JavaScript®
+for ( var value = object[0]; i < length && callback.call( value, i, value ) !== false; value = object[++i] ) {}
