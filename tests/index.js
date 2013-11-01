193cab4e6c1c45803a577d66f8474ce487b15894
Teddy Cross <tkazec@gmail.com>
2013-10-31 18:52:57 -0700

    Finish tests @_@

--- tests/index.js
+++ tests/index.js
@@ -1,22 +1,48 @@
 var assert = require('assert');
 var async = require('async');
+var crypto = require('crypto');
+var csso = require('csso');
 var ff = require('ff');
+var fixtures = require('test-fixtures');
 var shell = require('shelljs');
+var uglify = require('uglify-js');
 var ups = require('..');
 
-var files = {
-    basic: {"css":{},"js":{"global":"<script src='/ups/3a32d3bc8e-global.js'></script>"},"all":{"global.js":"3a32d3bc8e"}}
-  , basicMin: {"css":{},"js":{"global":"<script src='/ups/dbe4fdaf47-global.js'></script>"},"all":{"global.js":"dbe4fdaf47"}}
+shell.cd(__dirname);
+shell.rm('-r', 'publics');
+shell.mkdir('publics');
+
+var hash = function (data) {
+  return crypto.createHash('md5').update(data).digest('hex').slice(0, 10);
 };
 
-before(function () {
-  shell.cd(__dirname);
-  shell.mkdir('publics');
-});
+var files = {
+    global_css: fixtures.css.foundation.load() + fixtures.css.jqueryui.load()
+  , global_css_min: csso.justDoIt(fixtures.css.foundation.load()) + csso.justDoIt(fixtures.css.jqueryui.load())
+  , global_js: fixtures.js.ember.load() + ';' + fixtures.js.jqueryui.load() + ';'
+  , global_js_min: uglify.minify(fixtures.js.ember.load(), { fromString: true }).code + uglify.minify(fixtures.js.jqueryui.load(), { fromString: true }).code
+  , index_css: shell.cat('sources/css.css')
+  , index_css_min: csso.justDoIt(shell.cat('sources/css.css'))
+  , index_js: shell.cat('sources/js.js') + ';'
+  , index_js_min: uglify.minify(shell.cat('sources/js.js'), { fromString: true }).code
+};
 
-after(function () {
-  shell.rm('-r', 'publics');
-});
+var hashes = {
+    global_css: hash(files.global_css)
+  , global_css_min: hash(files.global_css_min)
+  , global_js: hash(files.global_js)
+  , global_js_min: hash(files.global_js_min)
+  , index_css: hash(files.index_css)
+  , index_css_min: hash(files.index_css_min)
+  , index_js: hash(files.index_js)
+  , index_js_min: hash(files.index_js_min)
+};
+
+var manifests = {
+    basic: {"css":{"global":"<link rel='stylesheet' href='/ups/"+hashes.global_css+"-global.css'>","index":"<link rel='stylesheet' href='/ups/"+hashes.index_css+"-index.css'>"},"js":{"global":"<script src='/ups/"+hashes.global_js+"-global.js'></script>","index":"<script src='/ups/"+hashes.index_js+"-index.js'></script>"},"all":{"global.css":hashes.global_css,"global.js":hashes.global_js,"index.css":hashes.index_css,"index.js":hashes.index_js}}
+  , basicMin: {"css":{"global":"<link rel='stylesheet' href='/ups/"+hashes.global_css_min+"-global.css'>","index":"<link rel='stylesheet' href='/ups/"+hashes.index_css_min+"-index.css'>"},"js":{"global":"<script src='/ups/"+hashes.global_js_min+"-global.js'></script>","index":"<script src='/ups/"+hashes.index_js_min+"-index.js'></script>"},"all":{"global.css":hashes.global_css_min,"global.js":hashes.global_js_min,"index.css":hashes.index_css_min,"index.js":hashes.index_js_min}}
+  , types: {"css":{"css":"<link rel='stylesheet' href='/ups/f13c2c05cf-css.css'>","less":"<link rel='stylesheet' href='/ups/b83abab1da-less.css'>","stylus":"<link rel='stylesheet' href='/ups/1349d685e3-stylus.css'>","nib":"<link rel='stylesheet' href='/ups/25169cfd91-nib.css'>"},"js":{"coffeescript":"<script src='/ups/4e68e71cbf-coffeescript.js'></script>","handlebars":"<script src='/ups/811452a0ba-handlebars.js'></script>","ember":"<script src='/ups/6428bda773-ember.js'></script>","js":"<script src='/ups/024f88cca3-js.js'></script>"},"all":{"coffeescript.js":"4e68e71cbf","css.css":"f13c2c05cf","handlebars.js":"811452a0ba","ember.js":"6428bda773","js.js":"024f88cca3","less.css":"b83abab1da","stylus.css":"1349d685e3","nib.css":"25169cfd91"}}
+};
 
 describe('ups.build()', function () {
   this.timeout(0);
@@ -25,7 +51,12 @@ describe('ups.build()', function () {
     var f = ff(function () {
       ups.build(__dirname + '/configs/basic.json', false, f.wait());
     }, function () {
-      assert.deepEqual(JSON.parse(shell.cat('publics/manifest.json')), files.basic);
+      assert.deepEqual(JSON.parse(shell.cat('publics/manifest.json')), manifests.basic);
+      
+      assert.equal(shell.cat('publics/' + hashes.global_css + '-global.css'), files.global_css);
+      assert.equal(shell.cat('publics/' + hashes.global_js + '-global.js'), files.global_js);
+      assert.equal(shell.cat('publics/' + hashes.index_css + '-index.css'), files.index_css);
+      assert.equal(shell.cat('publics/' + hashes.index_js + '-index.js'), files.index_js);
     }).onComplete(next);
   });
   
@@ -33,28 +64,12 @@ describe('ups.build()', function () {
     var f = ff(function () {
       ups.build(__dirname + '/configs/basic.json', true, f.wait());
     }, function () {
-      assert.deepEqual(JSON.parse(shell.cat('publics/manifest.json')), files.basicMin);
-    }).onComplete(next);
-  });
-  
-  it('should be deterministic', function (next) {
-    async.timesSeries(10, function (n, next) {
-      var f = ff(function () {
-        ups.build(__dirname + '/configs/basic.json', false, f.wait());
-      }, function () {
-        assert.deepEqual(JSON.parse(shell.cat('publics/manifest.json')), files.basic);
-      }).onComplete(next);
-    }, next);
-  });
-  
-  it('should customize group strings', function (next) {
-    var f = ff(function () {
-      ups.build(__dirname + '/configs/gstrs.json', false, f.wait());
-    }, function () {
-      var manifest = JSON.parse(shell.cat('publics/manifest.json'));
+      assert.deepEqual(JSON.parse(shell.cat('publics/manifest.json')), manifests.basicMin);
       
-      assert.equal(manifest.css.global, '/ups/3672c5f0b6-global.css');
-      assert.equal(manifest.js.global, '/ups/e3265f70cc-global.js');
+      assert.equal(shell.cat('publics/' + hashes.global_css_min + '-global.css'), files.global_css_min);
+      assert.equal(shell.cat('publics/' + hashes.global_js_min + '-global.js'), files.global_js_min);
+      assert.equal(shell.cat('publics/' + hashes.index_css_min + '-index.css'), files.index_css_min);
+      assert.equal(shell.cat('publics/' + hashes.index_js_min + '-index.js'), files.index_js_min);
     }).onComplete(next);
   });
 });
@@ -74,7 +89,7 @@ describe('ups.clean()', function () {
       
       ups.clean(__dirname + '/configs/basic.json', f.wait());
     }, function () {
-      assert.deepEqual(shell.ls('-A', 'publics'), ['3a32d3bc8e-global.js', 'manifest.json']);
+      assert.deepEqual(shell.ls('-A', 'publics').sort(), [hashes.global_css + '-global.css', hashes.global_js + '-global.js', hashes.index_css + '-index.css', hashes.index_js + '-index.js', 'manifest.json'].sort());
     }).onComplete(next);
   });
   
@@ -85,7 +100,7 @@ describe('ups.clean()', function () {
       
       ups.clean(__dirname + '/configs/basic.json', f.wait());
     }, function () {
-      assert.deepEqual(shell.ls('-A', 'publics'), ['.dotfile', '.placeholder', '3a32d3bc8e-global.js', 'manifest.json']);
+      assert.deepEqual(shell.ls('-A', 'publics').sort(), ['.dotfile', '.placeholder', hashes.global_css + '-global.css', hashes.global_js + '-global.js', hashes.index_css + '-index.css', hashes.index_js + '-index.js', 'manifest.json'].sort());
     }).onComplete(next);
   });
 });
@@ -113,49 +128,49 @@ describe('ups/types', function () {
   
   describe('coffeescript', function () {
     it('should render CoffeeScript', function () {
-      assert.equal(manifest.all['coffeescript.js'], 'a638d3300f');
+      assert.equal(manifest.all['coffeescript.js'], manifests.types.all['coffeescript.js']);
     });
   });
   
   describe('css', function () {
     it('should render CSS', function () {
-      assert.equal(manifest.all['css.css'], '3672c5f0b6');
+      assert.equal(manifest.all['css.css'], manifests.types.all['css.css']);
     });
   });
   
   describe('handlebars', function () {
     it('should render Handlebars', function () {
-      assert.equal(manifest.all['handlebars.js'], 'd656caf90f');
+      assert.equal(manifest.all['handlebars.js'], manifests.types.all['handlebars.js']);
     });
   });
   
   describe('handlebars-ember', function () {
     it('should render Emberized Handlebars', function () {
-      assert.equal(manifest.all['ember.js'], '45b4077ffc');
+      assert.equal(manifest.all['ember.js'], manifests.types.all['ember.js']);
     });
   });
   
   describe('js', function () {
     it('should render JS', function () {
-      assert.equal(manifest.all['js.js'], 'e3265f70cc');
+      assert.equal(manifest.all['js.js'], manifests.types.all['js.js']);
     });
   });
   
   describe('less', function () {
     it('should render LESS', function () {
-      assert.equal(manifest.all['less.css'], '20016d933a');
+      assert.equal(manifest.all['less.css'], manifests.types.all['less.css']);
     });
   });
   
   describe('stylus', function () {
     it('should render Stylus', function () {
-      assert.equal(manifest.all['stylus.css'], '4bf19df04b');
+      assert.equal(manifest.all['stylus.css'], manifests.types.all['stylus.css']);
     });
   });
   
   describe('stylus-nib', function () {
     it('should render Stylus with Nib', function () {
-      assert.equal(manifest.all['nib.css'], '7589b54712');
+      assert.equal(manifest.all['nib.css'], manifests.types.all['nib.css']);
     });
   });
 });
