72ddcd6b39e59d3890f061dc429e0e1e341fdfc2
cdelauder <cdelauder@gmail.com>
2015-06-19 09:22:55 -0700

    added the ability to only build css or js if the type variable is passed as a parameter.

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ module.exports.types = {
   , js: require('./types/js')
 };
 
-module.exports.build = function (config, minify, next) {
+module.exports.build = function (config, minify, next, type) {
   var assets = JSON.parse(fs.readFileSync(config, 'utf8'));
+  if (type) {
+    var originalManifest = JSON.parse(fs.readFileSync(path.join(assets._out, 'manifest.json'), 'utf8'));
+  }
   var manifest = {
       css: {}
     , js: {}
@@ -23,10 +26,25 @@ module.exports.build = function (config, minify, next) {
     , out: path.join(config, assets._out)
     , web: assets._web
     , str: {
-          css: (assets._str && assets._str.css) || '<link rel=\'stylesheet\' href=\'%%\'>'
-        , js: (assets._str && assets._str.js) || '<script src=\'%%\'></script>'
+        css: (assets._str && assets._str.css) || '<link rel=\'stylesheet\' href=\'%%\'>'
+      , js: (assets._str && assets._str.js) || '<script src=\'%%\'></script>'
       }
   };
+  if (type) {
+    manifest = originalManifest;
+    manifest[type] = {};
+    async.eachSeries(Object.keys(manifest.all), function (key, next) {
+      if (key.indexOf(type) >= 0) {
+        delete manifest.all[key];
+      };
+      next();
+    }, function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+
   
   async.eachSeries(Object.keys(assets), function (group, next) {
     if (!Array.isArray(assets[group])) {
@@ -36,6 +54,9 @@ module.exports.build = function (config, minify, next) {
     async.eachSeries(assets[group], function (name, next) {
       var proc = module.exports.types[path.extname(name).slice(1)];
       var file = path.join(config.src, name);
+      if (type && path.extname(name).slice(1) !== type) {
+        return next();
+      }
       
       if (!proc) {
         return next('no type for ' + name);
@@ -60,21 +81,27 @@ module.exports.build = function (config, minify, next) {
       }
       
       var f = ff(function () {
-        var distcss = manifest.css[group];
-        var distjs = manifest.js[group];
+        if (!type) {
+          var distcss = manifest.css[group];
+          var distjs = manifest.js[group];
+        } else if (type === 'css') {
+          var distcss = manifest.css[group];
+        } else if (type === 'js') {
+          var distjs = manifest.js[group];
+        }
         
         if (distcss) {
           var hashcss = crypto.createHash('md5').update(distcss).digest('hex').slice(0, 10);
-          manifest.css[group] = config.str.css.replace('%%', config.web + '/' + hashcss + '-' + group + '.css');
-          manifest.all[group + '.css'] = hashcss;
-          fs.writeFile(path.join(config.out, hashcss + '-' + group + '.css'), distcss, f.wait());
+          manifest.css[group] = config.str.css.replace('%%', config.web + '/dist-' + group + '.css');
+          manifest.all[group + '.css'] = 'dist-' + group + '.css';
+          fs.writeFile(path.join(config.out, 'dist-' + group + '.css'), distcss, f.wait());
         }
         
         if (distjs) {
           var hashjs = crypto.createHash('md5').update(distjs).digest('hex').slice(0, 10);
-          manifest.js[group] = config.str.js.replace('%%', config.web + '/' + hashjs + '-' + group + '.js');
-          manifest.all[group + '.js'] = hashjs;
-          fs.writeFile(path.join(config.out, hashjs + '-' + group + '.js'), distjs, f.wait());
+          manifest.js[group] = config.str.js.replace('%%', config.web + '/dist-' + group + '.js');
+          manifest.all[group + '.js'] = 'dist-' + group + '.js';
+          fs.writeFile(path.join(config.out, 'dist-' + group + '.js'), distjs, f.wait());
         }
       }).onComplete(next);
     });
@@ -82,7 +109,6 @@ module.exports.build = function (config, minify, next) {
     if (err) {
       return next(err);
     }
-    
     fs.writeFile(path.join(config.out, 'manifest.json'), JSON.stringify(manifest), next);
   });
 };
